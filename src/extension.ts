a52b855165b80cbbdbdec5c6843b39e40630f50d
Atheer Auribi <atheer.auribi@gmail.com>
2023-12-06 22:08:10 -0600

    implemented openLabInstructions opens readme in root directory of workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -25,8 +27,40 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.env.openExternal(vscode.Uri.parse(`https://localhost:${VNC_PORT}`));
 	})
 
+	let openLabInstructionsCmd = vscode.commands.registerCommand('lab-buddy.openLabInstructions', () => {
+		vscode.window.showInformationMessage('Opening Lab Instructions...');
+		// Get the path to the root of the workspace without using any deprecated APIs
+		const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+
+        if (rootPath) {
+            // Construct the path to the README.md file
+            const readmePath = path.join(rootPath, 'README.md');
+
+            // Check if the README.md file exists
+            if (fs.existsSync(readmePath)) {
+                // Read the contents of the README.md file
+                const readmeContent = fs.readFileSync(readmePath, 'utf-8');
+
+                // Show the README content in a preview
+                vscode.commands.executeCommand('markdown.showPreview', vscode.Uri.file(readmePath), {
+                    content: readmeContent,
+                    title: 'README.md Preview'
+                });
+            } else {
+                vscode.window.showErrorMessage('README.md file not found in the root workspace directory.');
+            }
+        } else {
+            vscode.window.showErrorMessage('No workspace is open.');
+        }
+	})
+
+	
+
+
+
 	context.subscriptions.push(helloWorldCmd);
 	context.subscriptions.push(launchVncCmd);
+	context.subscriptions.push(openLabInstructionsCmd);
 }
 
 // This method is called when your extension is deactivated
