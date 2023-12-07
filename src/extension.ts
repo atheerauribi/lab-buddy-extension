fd463e4f8c5aefda796b7ae78f7de181ea04a75d
Atheer Auribi <atheer.auribi@gmail.com>
2023-12-06 21:37:44 -0600

    first implementation of launchVnc

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const VNC_PORT = 5900;
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,13 +14,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lab-buddy.helloWorld', () => {
+	let helloWorldCmd = vscode.commands.registerCommand('lab-buddy.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Lab Buddy!');
 	});
 
-	context.subscriptions.push(disposable);
+	let launchVncCmd = vscode.commands.registerCommand('lab-buddy.launchVNC', () => {
+		vscode.window.showInformationMessage('Launching VNC Viewer...');
+		vscode.env.openExternal(vscode.Uri.parse(`localhost:${VNC_PORT}`));
+	})
+
+	context.subscriptions.push(helloWorldCmd);
+	context.subscriptions.push(launchVncCmd);
 }
 
 // This method is called when your extension is deactivated
