df6209c481aa5a4e21b57cd8a480b72f2315fae9
Atheer Auribi <atheer.auribi@gmail.com>
2023-12-10 20:28:04 -0600

    cleaned up code, added comments

--- src/extension.ts
+++ src/extension.ts
@@ -3,38 +3,16 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-// const pdf2md = require('@opendocsg/pdf2md');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const VNC_PORT = 5900;
 
-		//helper function that converts a pdf file in root directory to a markdown file
-		// async function convertPdfToMd (pdfPath: string) {
-		// 	//use pdf2md library to convert pdf in root folder to instructions.md markdown file, fully replacing it
-
-		// 	const pdfBuffer = fs.readFileSync(pdfPath);
-		// 	console.log("converting instructions to markdown...");
-		// 	pdf2md(pdfBuffer).then((text: any) => {
-		// 		// Write the markdown file to the root directory
-		// 		fs.writeFileSync(path.join(vscode.workspace.workspaceFolders?.[0].uri.fsPath!, 'instructions.md'), text);
-		// 	})		
-		// }
-	// Create a status bar item
-
-
-
 	async function openLabInstructions() {
 		vscode.window.showInformationMessage('Opening Lab Instructions...');
 		// Get the path to the root of the workspace
 		const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-
-		// check if there is a pdf file in the root directory
-		// if there is, convert it to a markdown file using convertPdfToMd()
-		// if (fs.existsSync(path.join(rootPath!, 'instructions.pdf'))) {
-		// 	await convertPdfToMd(path.join(rootPath!, 'instructions.pdf'));
-		// }	
 
         if (rootPath) {
             // Construct the path to the README.md file
@@ -56,38 +34,14 @@ export function activate(context: vscode.ExtensionContext) {
                 vscode.window.showErrorMessage('README.md file not found in the root workspace directory.');
             }
 
-			// // Check if the instructions.md file exists
-			// if (fs.existsSync(instructionsPath)) {
-			// 	// Read the contents of the instructions.md file
-			// 	const instructionsContent = fs.readFileSync(instructionsPath, 'utf-8');
-
-			// 	// Show the instructions content in a preview
-			// 	vscode.commands.executeCommand('markdown.showPreview', vscode.Uri.file(instructionsPath), {
-			// 		content: instructionsContent,
-			// 		title: 'instructions.md Preview'
-			// 	});
-			// } else {
-			// 	vscode.window.showErrorMessage('instructions.md file not found in the root workspace directory.');
-			// }
         } else {
             vscode.window.showErrorMessage('No workspace is open.');
         }
 	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lab-buddy" is now active!');
 
 	openLabInstructions();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let helloWorldCmd = vscode.commands.registerCommand('lab-buddy.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Lab Buddy!');
-	});
 
 	// Register the command to launch VNC Viewer
 	// Opens the VNC Viewer in the default browser, directing to localhost:5900
@@ -96,6 +50,8 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.env.openExternal(vscode.Uri.parse(`https://localhost:${VNC_PORT}`));
 	})
 
+	// Register the command to open the lab instructions
+	// Opens the README.md file in a preview window
 	let openLabInstructionsCmd = vscode.commands.registerCommand('lab-buddy.openLabInstructions', () => {
 		vscode.window.showInformationMessage('Opening Lab Instructions...');
 		// Get the path to the root of the workspace
@@ -126,16 +82,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	// Adding commands to the context
-	context.subscriptions.push(helloWorldCmd);
 	context.subscriptions.push(launchVncCmd);
 	context.subscriptions.push(openLabInstructionsCmd);
 
+
+	// Create a status bar item
 	const myButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 
     // Set properties for the button
     myButton.text = "$(vm-running) Launch VNC";
     myButton.tooltip = "Launch VNC Viewer in default browser";
-    myButton.command = "lab-buddy.launchVNC"; // This command should be defined in package.json
+    myButton.command = "lab-buddy.launchVNC"; 
 
 	// Show the button
 	myButton.show();
